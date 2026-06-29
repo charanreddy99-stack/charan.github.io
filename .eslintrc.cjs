7ac4a2b767ac9bc069be05fcaf789f1453217156
charanreddy99-stack <charanreddygl99@gmail.com>
2026-06-30 01:51:48 +0530

    Add files via upload

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: { node: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-types': 'warn',
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
+}
